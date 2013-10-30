3a61cf10c9ccc9cb57e0adcf6766a38f7b1ba533
koty <kouichi.nishizawa+github@gmail.com>
2013-10-30 13:03:09 +0900

    1st commit

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Livet;
+using WpfPrintReportSample.ViewModels;
+using WpfPrintReportSample.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfPrintReportSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        protected void Application_Startup(object sender, StartupEventArgs e)
+        {
+            DispatcherHelper.UIDispatcher = Dispatcher;
+
+            var vm = new MainWindowViewModel();
+            var window = new MainWindow();
+            window.DataContext = vm;
+            window.Show();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Livet;
+using Livet.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrintReportSample.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Text = "あいうえお";
+        }
+
+        public string Text { get; set; }
+
+        private ViewModelCommand printCommand;
+        public ViewModelCommand PrintCommand
+        {
+            get
+            {
+                return this.printCommand ?? (this.printCommand = new ViewModelCommand(this.Print));
+            }
+        }
+
+        private void Print()
+        {
+
+        }
+    }
+}
